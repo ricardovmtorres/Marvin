5d14b0ac4fb1bca37aa6090b416c5a55f05dce71
ricardovmtorres <38121823+ricardovmtorres@users.noreply.github.com>
2022-01-22 17:06:54 -0300

    init google search

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 ################################
 
 # ---WARNINGS---
-# This is a Marvin may new Python son.
+# This is a Marvin my new Python son.
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -12,12 +12,37 @@
 # IMPORTS
 import speech_recognition as sr
 import pyttsx3
+from googlesearch import search
 
-
-# FUNCTIONS
+### FUNCTIONS ###
+## Utils ##
 def begin():
     x = 1
 
+def google_search(command):
+    for result in search()
+        return "google"
+    return 1
+
+def build_phrase(words):
+    frase: str = ''
+    for word in words:
+        frase += word
+    return frase
+
+
+## ROBOTO ##
+# Acha função
+def find_func(command):
+    command_sliced = command.slice()
+    if 'marvin' in command:
+        print("Você disse: '" + command + "'")
+        if command_sliced[1] == 'google':
+            google_search(command)
+
+    else:
+        return 1
+    return 0
 
 # Abre o microfone e reconhece pt-BR
 def ouvir_mic():
@@ -29,28 +54,30 @@ def ouvir_mic():
             voice = listener.listen(source)
             command = listener.recognize_google(voice, language='pt-BR')
             command = command.lower()
-            print("Voce disse: "+command+"?")
-            if 'marvin' in command:
-                print(command)
+            # Quando for estudar erros e acertos, sempre marcar acerto
+            # a não ser que o proximo comando indique leitura errada
+            # print("Voce disse: "+command+"?")
+            find_func(command)
     except sr.UnknownValueError:
         return "Não entendi humano... :? "
     finally:
         return 42
 
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Olá! Meu nome é {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# MAIN
+### MAIN ###
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # object speech recognition creation
-    # habilita e configura o microfone
     listener = sr.Recognizer()
     # object text to speech creation
     engine = pyttsx3.init()
+    # start computing
     begin()
+    # first interaction
     print_hi('Marvin')
+    # start listening
+
     print(ouvir_mic())
